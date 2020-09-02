87cf6b4deb0489644999d597761b0c3f2ccb97b4
Máté Lenkey <lenkeyl.mm@gmail.com>
2020-09-02 11:34:05 +0200

    Homescreen headbar

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <img class='background' src='bg.svg'>
+    <p class='logo'>Feladat</p>
+    <p class='menu_element_0'>Home</p>
+    <p class='menu_element_1'>Form</p>
+    <p class='menu_element_2'>About</p>
+    <p class='menu_element_3'>Contact</p>
+</body>
+</html>
